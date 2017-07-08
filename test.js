78146c03cd115fa287079ea746497ba7929ba3d9
Felipe César <jeanfelipecc@gmail.com>
2017-07-08 15:55:53 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,31 @@
+var phantomcss = require('phantomcss');
+
+// start a casper test
+casper.test.begin('Tags', function(test) {
+
+  phantomcss.init({
+    rebase: casper.cli.get('rebase')
+  });
+
+  // open page
+  casper.start('http://127.0.0.1:8080/');
+
+  // set your preferred view port size
+  casper.viewport(1024, 768);
+
+  casper.then(function() {
+      // take the screenshot of the whole body element and save it under "body.png". The first parameter is actually a CSS selector
+      phantomcss.screenshot('.wrapper', 'wrapper');
+  });
+
+  casper.then(function now_check_the_screenshots() {
+    // compare screenshots
+    phantomcss.compareAll();
+  });
+
+  // run tests
+  casper.run(function() {
+    console.log('\nTHE END.');
+    casper.test.done();
+  });
+});
